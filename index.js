259198f54ece078f2bd3d82b683292c010f43897
Adriana <am20dipi@alum.siena.edu>
2021-05-21 18:16:16 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (input) => {
+  return tutorials.map( sentence => {
+      const splitSentences = sentence.split(' ');
+    // split each line using .split(' ');
+    // set equal to a constant
+      const capSentences = 
+          splitSentences.map( sentence => sentence.charAt(0).toUpperCase() + sentence.slice(1));
+      const response = capSentences.join(" ");
+      return response;
+
+
+    // take constant and call map on it
+    // map's arguments are for each first character of each word uppercase it and replace it using slice.
+    // set constant equal to above
+
+    // use .join(' ') and call on altered collection
+
+    //return collection
+  });
 }
